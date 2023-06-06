ee8af69e3bd3a371364d142718f023261673c6af
Varun Aggarwal (Corporate <kmbl279646@KBLT22-00064341.local>
2023-06-06 14:45:12 +0530

    Visual Change to fileUpload component, added kotak banner

--- src/App.js
+++ src/App.js
@@ -1,30 +1,40 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import Banner from "./Banner";
 
 function App() {
-    const { register, handleSubmit } = useForm();
+  const { register, handleSubmit } = useForm();
 
-    const onSubmit = async (data) => {
-        const formData = new FormData();
-        formData.append("file", data.file[0]);
+  const onSubmit = async (data) => {
+    const formData = new FormData();
+    formData.append("file", data.file[0]);
 
-        const res = await fetch("http://localhost:8080/api/csv/upload", {
-            method: "POST",
-            body: formData,
-        }).then((res) => res.json());
-        alert(JSON.stringify(`${res.message}, status: ${res.status}`));
-    };
+    const res = await fetch("http://localhost:8080/api/csv/upload", {
+      method: "POST",
+      body: formData,
+    }).then((res) => res.json());
+    alert(JSON.stringify(`${res.message}, status: ${res.status}`));
+  };
 
-
-    return (
-        <div className="box">
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <h3>Upload CSV Files less then 50 MB</h3>
-                <input type="file" {...register("file")} />
-                <input type="submit" />
-            </form>
-        </div>
-    );
+  return (
+    <div className = "App">
+      <Banner />
+      <div className="box">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <h3>Upload CSV Files less then 50 MB</h3>
+          <div class="mb-4 mt-4">
+            <input
+              class="form-control"
+              type="file"
+              id="formFile"
+              {...register("file")}
+            ></input>
+          </div>
+          <input type="submit" />
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Banner.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <div class="banner">
+      <img src={require('./kotak-logo.png')} alt="Kotak Mahindra Bank Logo" class="logo" />
+    </div>
+  );
+};
+
+export default Banner;
